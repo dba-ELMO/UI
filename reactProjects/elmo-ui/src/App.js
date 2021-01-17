a84b89a7786cb52d502cedcb24c94fed28944dc2
unknown <alish1999@gmail.com>
2021-01-18 00:35:45 +0200

    with yoni's face

---
+++ reactProjects/elmo-ui/src/App.js
@@ -0,0 +1,29 @@
+// import logo from '../public/elmo_head.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="App-header">
+        <button>
+        <img src="elmo_head.png" className="App-logo" alt="logo" />
+        </button>
+        
+        <img src="doch1.PNG" className="App-logo" alt="logo" />
+        <p>
+          click the elmo ckjvdsh to login
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </body>
+    </div>
+  );
+}
+
+export default App;
