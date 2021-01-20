fc3ee7043474af9d9aa94cb1639fc16f4db34c36
maayan mordehai <maayanmordehai3@gmail.com>
2021-01-20 23:50:24 +0200

    fixed var names

--- elmo/src/HomePage.js
+++ elmo/src/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
     let firstNav=[{"text": "Projects", "href": "#projects" }, {"text": "Users", "href": "#users"}, {"text": "Postgresql", "href": "#postgres" }, {"text": "Oracle", "href": "#oracle"}, {"text": "Mongodb", "href": "#mongo"}];
     let secondNav=[{text: "Login", href: "#login"}];
     return (
-        <ElmoNav firstNav={mylist} secondNav={secondlist}/>
+        <ElmoNav firstNav={firstNav} secondNav={secondNav}/>
     );
   }
   
